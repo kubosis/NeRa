e2dcf9128f7d7d8c9124892403fba194d6ff8684
kubosis <kubasukdol@gmail.com>
2024-02-07 23:17:25 +0100

    testing models

--- PyPredict/models/models.py
+++ PyPredict/models/models.py
@@ -13,9 +13,9 @@ target_dim = 3
 
 class GCONVCheb(nn.Module):
     def __init__(self, team_count: int,
-                 embed_dim: int = 10,
+                 embed_dim: int = 20,
                  dense_dims: tuple[int] = (8, 8, 8, 8, 8),
-                 conv_out_dim: int = 16,
+                 conv_dims: tuple[int] = (16, 16, 16),
                  dropout_rate: float = 0.1,
                  activation: str = 'relu',
                  K: int = 5):
@@ -24,15 +24,18 @@ class GCONVCheb(nn.Module):
 
         self.team_count = team_count
 
+        self.act_fn = _activations[activation]
+
         self.embed_dim = embed_dim
         self.embedding = nn.Embedding(num_embeddings=team_count, embedding_dim=embed_dim)
 
-        self.gconv = GConvGRU(embed_dim, conv_out_dim, K, 'sym')
-
-        self.activation = _activations[activation]
+        gconv = [GConvGRU(embed_dim, conv_dims[0], K)]
+        for i in range(1, len(conv_dims)):
+            gconv.append(GConvGRU(conv_dims[i - 1], conv_dims[i], K))
+        self.gconv_layers = gconv
 
         dense = nn.Sequential()
-        dense.add_module('lin0', nn.Linear(in_features=conv_out_dim * 2, out_features=dense_dims[0]))
+        dense.add_module('lin0', nn.Linear(in_features=conv_dims[-1] * 2, out_features=dense_dims[0]))
         dense.add_module('act0', _activations[activation])
         dense.add_module('dropout0', nn.Dropout(p=dropout_rate))
         for i in range(1, len(dense_dims)):
@@ -52,8 +55,11 @@ class GCONVCheb(nn.Module):
 
         h = torch.tensor(list(range(self.team_count)))
         h = self.embedding(h).reshape(-1, self.embed_dim)
-        h = self.gconv(h, edge_index)
-        h = self.activation(h)
+
+        for gconv in self.gconv_layers:
+            h = gconv(h, edge_index)
+            h = self.act_fn(h)
+
         h = torch.cat([h[home], h[away]], dim=1)
         h = self.dense_sequence(h)
         h = self.out_layer(h)
--- PyPredict/trainer.py
+++ PyPredict/trainer.py
@@ -1,6 +1,7 @@
 from typing import Union
 from datetime import timedelta
 
+import numpy as np
 import pandas as pd
 from loguru import logger
 import torch
@@ -31,33 +32,49 @@ class Trainer:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(device)
 
-    def train(self, epochs: int = 10, batch_size: int = 64, verbose: bool = False):
-        self.model.train()
-
+    def train(self, epochs: int = 100, batch_size: int = 64, verbose: bool = False) -> np.ndarray:
+        training_accuracy = []
         for epoch in range(epochs):
+            self.model.train()
             self.optimizer.zero_grad()
-            accuracy = 0
-            loss = 0
-            count = 0
+            accuracy, loss, count = 0, 0, 0
             for time, snapshot in enumerate(self.train_dataset):
                 y_hat = self.model(snapshot.edge_index)
                 y = snapshot.edge_attr  # edge weight encodes the match outcome
 
                 target = torch.argmax(y, dim=1)
-
+                prediction = torch.argmax(y_hat, dim=1)
                 cost = self.loss_fn(y_hat, target)
+                accuracy += int((prediction.eq(target)).sum().item())
+                count += len(prediction)
+
                 cost.backward()
                 self.optimizer.step()
                 loss += cost.item()
-                prediction = torch.argmax(y_hat, dim=1)
-                accuracy += int((prediction.eq(target)).sum().item())
-                count += len(prediction)
+
             if verbose:
-                logger.info(f'Epoch: {epoch}, training loss: {loss:.3f}, '
-                            f'accuracy: {accuracy/count * 100:.2f}%')
+                logger.info(f'[TRN] Epoch: {epoch}, training loss: {loss:.3f}, '
+                            f'training accuracy: {accuracy/count * 100:.2f}% '
+                            )
+            training_accuracy.append(accuracy/count * 100)
+        return np.array(training_accuracy)
 
+    def test(self, verbose: bool = False) -> float:
+        self.model.eval()
+        correct, count = 0, 0
+        with torch.no_grad():
+            for time, snapshot in enumerate(self.test_dataset):
+                y_hat = self.model(snapshot.edge_index)
+                y = snapshot.edge_attr
+                target = torch.argmax(y, dim=1)
+                prediction = torch.argmax(y_hat, dim=1)
+                correct += int((prediction.eq(target)).sum().item())
+                count += len(prediction)
+        test_accuracy = correct / count
+        if verbose:
+            logger.info(f'[TST] Testing accuracy: {100. * test_accuracy:.2f}%')
 
-
+        return test_accuracy
 
 
 
