6b9db8c346a3932937f47bc9bc4d869653d67496
kubosis <kubasukdol@gmail.com>
2024-02-24 12:52:31 +0100

    Dev: Elo manual pass

--- PyPredict/models/ratings/elo.py
+++ PyPredict/models/ratings/elo.py
@@ -1,4 +1,5 @@
 from typing import Sequence
+from collections.abc import Iterable
 
 import torch
 import torch.nn as nn
@@ -10,11 +11,8 @@ elo_params = {
     'd': int(400),
     'k': float(2)
 }
-
-Matches = Sequence[np.ndarray, np.ndarray]
-Match_outcome = np.ndarray  # 1 = home win, 1/2 draw, 0 = away win
-Goal_difference = np.ndarray
-Result: Sequence[Match_outcome, Goal_difference]
+Matches = Sequence[np.ndarray]
+Result = Sequence[np.ndarray]
 
 
 class EloManual(nn.Module):
@@ -33,24 +31,30 @@ class EloManual(nn.Module):
         for elem in elo_params:
             setattr(self, elem, kwargs[elem])
         self.rating = np.zeros((team_count,)) + default
-        self.E_H = self.E_A = 0
+        self.E_H = None
         self.home, self.away = None, None
 
     def forward(self, matches: Matches):
-        with torch.no_grad:
+        with torch.no_grad():
             self.home, self.away = matches
             home_rating = self.rating[self.home]
             away_rating = self.rating[self.away]
 
-            self.E_H = 1 / (1 + np.power(self.c, (home_rating - away_rating) / self.d))
+            self.E_H = 1 / (1 + np.power(self.c, ((away_rating - home_rating) / self.d)))
 
         return self.E_H
 
     def backward(self, result: Result):
-        with torch.no_grad:
+        with torch.no_grad():
             match_outcome, goal_difference = result
-            scale = self.k * ((1 + goal_difference)**self.gamma)
-            self.rating[self.home] += scale * (match_outcome - self.E_H)
-            self.rating[self.away] -= scale * (match_outcome - self.E_H)
+
+            h_i = self.home
+            a_i = self.away
+
+            update = self.k * ((1 + goal_difference)**self.gamma) * (match_outcome - self.E_H)
+
+            self.rating[h_i] += update
+            self.rating[a_i] -= update
 
         return result
+
