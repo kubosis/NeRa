87ed5134acc434b251ae2e4d5df630dfae4ba7c5
kubosis <kubasukdol@gmail.com>
2024-02-26 15:00:37 +0100

    fix: elo params moved to class

--- PyPredict/models/ratings/_elo/_model.py
+++ PyPredict/models/ratings/_elo/_model.py
@@ -4,19 +4,19 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-elo_params = {
-    'k': torch.tensor(3., dtype=torch.float64),
-    'gamma': torch.tensor(2., dtype=torch.float64),
-    'c': torch.tensor(3., dtype=torch.float64),
-    'd': torch.tensor(500., dtype=torch.float64),
-}
-
 Matches = Sequence[np.ndarray]
 Result = Sequence[np.ndarray]
 Index = int
 
 
 class EloModel(nn.Module):
+    _elo_params = {
+        'k': torch.tensor(3., dtype=torch.float64),
+        'gamma': torch.tensor(2., dtype=torch.float64),
+        'c': torch.tensor(3., dtype=torch.float64),
+        'd': torch.tensor(500., dtype=torch.float64),
+    }
+
     def __init__(self, team_count: int, default: float = 1000., **kwargs):
         """
         :param team_count: number of teams
@@ -28,11 +28,11 @@ class EloModel(nn.Module):
         """
 
         super(EloModel, self).__init__()
-        for elem in elo_params:
+        for elem in EloModel._elo_params:
             if elem in kwargs:
                 setattr(self, elem, kwargs[elem])
             else:
-                setattr(self, elem, elo_params[elem])
+                setattr(self, elem, EloModel._elo_params[elem])
 
         assert (isinstance(self.c, torch.Tensor) and isinstance(self.d, torch.Tensor))
 
