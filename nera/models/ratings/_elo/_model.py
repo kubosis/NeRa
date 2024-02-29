[fix]: hyper parameter gradient bool fix

nera/models/ratings/_elo/_model.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-from .._general_model import GeneralRating
+from .._general_rating import GeneralRating
 
 Matches = Sequence[np.ndarray]
 Result = Sequence[np.ndarray]

nera/models/ratings/_general_rating.py
@@ -7,8 +7,8 @@ class GeneralRating(nn.Module):
         super(GeneralRating, self).__init__()
 
         self.hp_grad = False  # compute gradient of hyper params?
-        if 'hp_grad' in params:
-            self.hp_grad = params['hp_grad']
+        if 'hp_grad' in kwargs:
+            self.hp_grad = kwargs['hp_grad']
 
         for elem in params.keys():
             if elem in kwargs: