4d6c87305b7041eacfd152692aa25551d807d4ba
kubosis <kubasukdol@gmail.com>
2024-02-29 13:28:52 +0100

    [dev]: general rating

--- nera/models/ratings/_elo/_model.py
+++ nera/models/ratings/_elo/_model.py
@@ -20,36 +20,8 @@ class EloModel(GeneralRating):
     }
 
     def __init__(self, team_count: int, default: float = 1000., **kwargs):
-        """
-        :param team_count: number of teams
-        :param default: default rating of all teams
-        :keyword gamma: impact scale of goal difference
-        :keyword c: rating meta parameter
-        :keyword d: rating meta parameter
-        :keyword k: learning rate
-        """
-
         super(EloModel, self).__init__(self._params, **kwargs)
 
-        self.cd_grad = False
-        if 'cd_grad' in kwargs:
-            self.cd_grad = kwargs['cd_grad']
-
-        if not isinstance(self.c, torch.Tensor):
-            self.c = torch.tensor(self.c, dtype=torch.float64)
-        if not isinstance(self.d, torch.Tensor):
-            self.d = torch.tensor(self.d, dtype=torch.float64)
-
-        if self.cd_grad:
-            self.c.requires_grad = True
-            self.d.requires_grad = True
-            self.c = nn.Parameter(self.c)
-            self.d = nn.Parameter(self.d)
-        else:
-            self.c = self.c.detach()
-            self.d = self.d.detach()
-
         self.rating = nn.Parameter(torch.full((team_count,), default, dtype=torch.float64))
-
         self.E_H = None
 
--- nera/models/ratings/_general_model.py
+++ nera/models/ratings/_general_model.py
@@ -1,12 +1,29 @@
 import torch.nn as nn
+import torch
 
 
 class GeneralRating(nn.Module):
     def __init__(self, params: dict, **kwargs):
         super(GeneralRating, self).__init__()
+
+        self.hp_grad = False  # compute gradient of hyper params?
+        if 'hp_grad' in params:
+            self.hp_grad = params['hp_grad']
+
         for elem in params.keys():
             if elem in kwargs:
-                setattr(self, elem, kwargs[elem])
+                par = kwargs[elem]
+                if not isinstance(par, torch.Tensor):
+                    par = torch.tensor(par, dtype=torch.float64)
             else:
-                setattr(self, elem, params[elem])
+                par = params[elem]
+
+            if self.hp_grad:
+                par.requires_grad = True
+                par = nn.Parameter(par)
+            else:
+                par = par.detach()
+
+            setattr(self, elem, par)
+
         self.home, self.away = None, None
