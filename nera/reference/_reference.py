6ff949b6ff96d4b4bdfde0a99eab8a57800b02ae
kubosis <kubasukdol@gmail.com>
2024-03-04 15:29:03 +0100

    [dev]: reference is now computed from temporal dataset iteration

--- nera/reference/_reference.py
+++ nera/reference/_reference.py
@@ -6,43 +6,22 @@ from loguru import logger
 
 
 class RatingReference:
-    def __init__(self, matches: pd.DataFrame, team_mapping: dict):
+    def __init__(self, num_teams):
         """
         Class RatingReference
         Compute rating manually from matches dataframe
-        :param matches: (DataFrame)
-        :param team_mapping: (dict) mapping of teams to their ids
+        :param num_teams: (int) number of teams
         """
-        self._matches = (matches, team_mapping)
+        self.num_teams = num_teams
 
-    @property
-    def matches(self):
-        """ Matches property only returns the DataFrame """
-        return self._matches[0]
-
-    @matches.setter
-    def matches(self, value: tuple[pd.DataFrame, dict]):
-        _error_string = "Matches setter must be a tuple of DataFrame and dict"
-        if not isinstance(value, tuple):
-            raise TypeError(_error_string)
-        if len(value) != 2:
-            raise ValueError(_error_string)
-
-        matches, team_mapping = value
-        if not isinstance(matches, pd.DataFrame):
-            raise TypeError(_error_string)
-        if not isinstance(team_mapping, dict):
-            raise TypeError(_error_string)
-
-        self._matches = (matches, team_mapping)
-
-    def compute_reference(self, rating: str, **kwargs) -> Sequence[np.ndarray]:
+    def compute_reference(self, rating: str, temp_dataset, **kwargs) -> Sequence[np.ndarray]:
         """
         :param rating: (str) 'elo' / 'berrar' / 'pi'
+        :param temp_dataset: (Temporal Dataset Signal Iterator)
         :return: Sequence[ndarray] computed rating values
         """
         if rating == 'elo':
-            return self._elo_reference(**kwargs)
+            return self._elo_reference(temp_dataset, **kwargs)
         elif rating == 'berrar':
             raise NotImplementedError
         elif rating == 'pi':
@@ -51,7 +30,7 @@ class RatingReference:
             logger.error(f'Unknown rating {rating}')
             raise ValueError(f'Unknown rating {rating}')
 
-    def _elo_reference(self, elo_base: int = 1000, gamma: float = 2., c: float = 3., d: float = 500.,
+    def _elo_reference(self, temp_dataset, elo_base: int = 1000, gamma: float = 2., c: float = 3., d: float = 500.,
                        k: float = 3., verbose: bool = False, **kwargs) -> Sequence[np.ndarray]:
         """
         Compute elo rating manually from natches dataframe
@@ -63,34 +42,37 @@ class RatingReference:
         :param verbose: (bool) print
         :return: np.ndarray with elo values
         """
-        matches, mapping = self._matches
 
-        elo = np.zeros((len(mapping),)) + elo_base
+        elo = np.zeros((self.num_teams,)) + elo_base
         E_HS = []
         elos = []
-        for i in range(len(matches.index) - 1, -1, -1):
-            match_i = matches.iloc[i]
-            h_i = mapping[match_i['Home']]
-            a_i = mapping[match_i['Away']]
+        iter_ = 0
+        for time, snapshot in enumerate(temp_dataset):
+            matches = snapshot.edge_index
+            match_points = snapshot.match_points
 
-            E_h = 1 / (1 + np.power(c, ((elo[a_i] - elo[h_i]) / d)))
-            S_h = 1. if match_i['Winner'] == 'home' else 0. if match_i['Winner'] == 'away' else 1 / 2
+            for m in range(matches.shape[1]):
+                match = matches[:, m]
 
-            E_HS.append(E_h)
+                h_i, a_i = match
+                home_pts, away_pts = match_points[m, 0], match_points[m, 1]
 
-            elos_b4 = (elo[h_i], elo[a_i])
+                E_h = 1 / (1 + np.power(c, ((elo[a_i] - elo[h_i]) / d)))
+                S_h = 1. if home_pts > away_pts else 0. if home_pts < away_pts else 1 / 2
 
-            h_points = match_i['Home_points']
-            a_points = match_i['Away_points']
+                E_HS.append(E_h)
 
-            delta = abs(h_points - a_points)
-            update = k * ((1 + delta) ** gamma) * (S_h - E_h)
+                elos_b4 = (elo[h_i], elo[a_i])
 
-            elo[h_i] += update
-            elo[a_i] -= update
+                delta = abs(home_pts - away_pts)
+                update = k * ((1 + delta) ** gamma) * (S_h - E_h)
 
-            elos.append((elos_b4, (elo[h_i], elo[a_i])))
+                elo[h_i] += update
+                elo[a_i] -= update
 
-            if verbose:
-                print(f"iteration {i}, rating: {elo}, E_H = {E_h}; {S_h}")
+                elos.append((elos_b4, (elo[h_i], elo[a_i])))
+
+                if verbose:
+                    print(f"iteration {iter_}, rating: {elo}, E_H = {E_h}; {S_h}")
+                iter_ += 1
         return [np.array(elo)]
