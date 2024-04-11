0ee4127a088b8ff65d6dfe5274486927bda8a4c7
kubosis <kubasukdol@gmail.com>
2024-04-11 17:26:56 +0200

    [dev]: gnn architectures

--- nera/models/gnn/_GConvRNN.py
+++ nera/models/gnn/_GConvRNN.py
@@ -1,8 +1,11 @@
+import copy
+
 import torch
+from torch import nn
 from torch_geometric.nn import GraphConv
 
 
-class GConvRNN(torch.nn.Module):
+class GConvElman(torch.nn.Module):
     r"""Our implementation of simple recurrent elman network for graph convolutional neural networks
 
     :math:`h_{t} = \sigma(W_{hx} *_g x_t + W_{hh} *_g h_{t-1} + b_h)`
@@ -26,57 +29,115 @@ class GConvRNN(torch.nn.Module):
             self,
             in_channels: int,
             out_channels: int,
-            K: int,
+            hidden_channels: int = -1,
             aggr: str = "add",
             bias: bool = True,
+            discount: float = 1.,
+            init_ones_: bool = True,
     ):
-        super(GConvRNN, self).__init__()
+        assert 0 <= discount <= 1
+        super(GConvElman, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.K = K
+        self.hidden_channels = hidden_channels if hidden_channels > 0 else out_channels
         self.aggr = aggr
         self.bias = bias
-        self.hts1 = None
-        self._create_conv_layers()
+        self.discount = discount
+        self.first_call = True
+        self.H = None
+        self.H_edge_index = None
+        self.H_edge_weight = None
+        self._create_conv_layers(init_ones_=init_ones_)
 
-    def _create_conv_layers(self):
+    def _create_conv_layers(self, init_ones_):
         self.conv_whx_x = GraphConv(
             in_channels=self.in_channels,
-            out_channels=self.out_channels,
+            out_channels=self.hidden_channels,
             bias=self.bias,
-            aggr=self.aggr
+            aggr=self.aggr,
+            flow="source_to_target",
         )
 
         self.conv_whh_hs1 = GraphConv(
-            in_channels=self.out_channels,
-            out_channels=self.out_channels,
+            in_channels=self.hidden_channels,
+            out_channels=self.hidden_channels,
             bias=self.bias,
-            aggr=self.aggr
+            aggr=self.aggr,
+            flow="source_to_target",
         )
 
         self.conv_wy_y = GraphConv(
-            in_channels=self.out_channels,
+            in_channels=self.hidden_channels,
             out_channels=self.out_channels,
             bias=self.bias,
-            aggr=self.aggr
+            aggr=self.aggr,
+            flow="source_to_target",
         )
 
+        if init_ones_:
+            nn.init.ones_(self.conv_whx_x.lin_rel.weight)
+            nn.init.ones_(self.conv_whx_x.lin_root.weight)
+            nn.init.ones_(self.conv_whh_hs1.lin_rel.weight)
+            nn.init.ones_(self.conv_whh_hs1.lin_root.weight)
+            nn.init.ones_(self.conv_wy_y.lin_rel.weight)
+            nn.init.ones_(self.conv_wy_y.lin_root.weight)
+
+            if self.conv_whx_x.lin_rel.bias is not None:
+                nn.init.zeros_(self.conv_whx_x.lin_rel.bias)
+            if self.conv_whx_x.lin_root.bias is not None:
+                nn.init.zeros_(self.conv_whx_x.lin_root.bias)
+
+            if self.conv_whh_hs1.lin_rel.bias is not None:
+                nn.init.zeros_(self.conv_whh_hs1.lin_rel.bias)
+            if self.conv_whh_hs1.lin_root.bias is not None:
+                nn.init.zeros_(self.conv_whh_hs1.lin_root.bias)
+
+            if self.conv_wy_y.lin_rel.bias is not None:
+                nn.init.zeros_(self.conv_wy_y.lin_rel.bias)
+            if self.conv_wy_y.lin_root.bias is not None:
+                nn.init.zeros_(self.conv_wy_y.lin_root.bias)
+
     def _set_hidden_state(self, X):
-        if self.hts1 is None:
-            return torch.zeros(X.shape[0], self.out_channels).to(X.device).float()
-        return self.hts1
+        if self.H is None:
+            return torch.ones(X.shape[0], self.out_channels).to(X.device).float()
+        return self.H
 
     def _calculate_ht(self, X, edge_index, edge_weight, H):
         Whx_g_xt = self.conv_whx_x(X, edge_index, edge_weight)
-        Whh_g_hts1 = self.conv_whh_hs1(H, edge_index, edge_weight)
-        ht = torch.sigmoid(Whx_g_xt + Whh_g_hts1)
-        return ht
+        Whh_g_hts1 = self.conv_whh_hs1(H, self.H_edge_index, self.H_edge_weight) if not self.first_call else 0
+        #Whh_g_hts1 = self.conv_whh_hs1(H, edge_index, edge_weight) #if not self.first_call else 0
+        H = torch.sigmoid(Whx_g_xt + Whh_g_hts1)
+        return H
 
-    def _calculate_yt(self, ht, edge_index, edge_weight):
-        Wy_g_ht = self.conv_wy_y(ht, edge_index, edge_weight)
+    def _calculate_yt(self, H, edge_index, edge_weight):
+        yt = Wy_g_ht = self.conv_wy_y(H, edge_index, edge_weight)
         yt = torch.sigmoid(Wy_g_ht)
         return yt
+
+    def _copy_hidden(self, ht):
+        self.H = ht.detach().clone()
+        self.H.requires_grad_(True)
+
+    def _copy_index(self, edge_index, edge_weight):
+        new_edge_index = edge_index.detach().clone()
+        if self.H_edge_index is None:
+            self.H_edge_index = new_edge_index
+        else:
+            self.H_edge_index = torch.cat([new_edge_index, self.H_edge_index], dim=1)
+
+        if edge_weight is None:
+            new_edge_weight = torch.ones_like(edge_index[0, :]).detach().to(torch.float)
+        else:
+            new_edge_weight = edge_weight.detach().clone().to(torch.float)
+        if self.H_edge_weight is None:
+            self.H_edge_weight = new_edge_weight
+        else:
+            self.H_edge_weight = torch.cat([new_edge_weight, self.H_edge_weight], dim=0)
+
+        self.H_edge_weight *= self.discount
+
+
 
     def forward(
             self,
@@ -97,7 +158,17 @@ class GConvRNN(torch.nn.Module):
         Return types:
             * **y** *(PyTorch Float Tensor)* - Output matrix.
         """
-        hts1 = self._set_hidden_state(X)
-        ht = self._calculate_ht(X, edge_index, edge_weight, hts1)
-        yt = self._calculate_yt(ht, edge_index, edge_weight)
+        H = self._set_hidden_state(X)
+
+        H = self._calculate_ht(X, edge_index, edge_weight, H)
+
+        # copy hidden state for next iter
+        self._copy_hidden(H)
+        self._copy_index(edge_index, edge_weight)
+
+        yt = self._calculate_yt(H, edge_index, edge_weight)# if not self.first_call else H
+        #yt = self._calculate_yt(H, self.H_edge_index, self.H_edge_weight)
+
+        self.first_call = False
+        #yt = self._calculate_yt(H, edge_index, edge_weight)
         return yt
--- nera/models/gnn/_simple_recurrent_GNN.py
+++ nera/models/gnn/_simple_recurrent_GNN.py
@@ -1,27 +1,41 @@
 import copy
+from typing import Optional
 
 import torch
 import torch.nn as nn
 
-from ._GConvRNN import GConvRNN
+from ._GConvRNN import GConvElman
+from ._gconv_gru_copy import GConvGRU
+
+
+#from torch_geometric_temporal.nn import GConvGRU
 
 
 class RGNN(nn.Module):
     _activations = {
         'relu': nn.ReLU(),
         'sigmoid': nn.Sigmoid(),
-        'lrelu': nn.LeakyReLU(0.2)
+        'lrelu': nn.LeakyReLU(0.2),
+        'tanh': nn.Tanh(),
     }
 
     def __init__(self, team_count: int,
                  embed_dim: int = 20,
                  dense_dims: tuple[int] = (8, 4),
                  conv_out_channels: int = 1,
+                 conv_hidden_channels: int = 1,
                  dropout_rate: float = 0.1,
-                 activation: str = 'lrelu',
+                 activation: str = 'relu',
                  target_dim: int = 3,
                  K: int = 1,
+                 bias: bool = True,
+                 init_ones_: bool = False,
+                 graph_conv: str = 'GCONV_GRU',
+                 normalization: Optional[str] = 'sym',
                  debug=False):
+
+        assert K > 0
+        assert graph_conv.upper() in ['GCONV_GRU', 'GCONV_ELMAN']
 
         super(RGNN, self).__init__()
 
@@ -33,7 +47,13 @@ class RGNN(nn.Module):
         #self.embedding = nn.Embedding(num_embeddings=team_count, embedding_dim=embed_dim)
 
         # recurrent graph convolution layer
-        gconv = GConvRNN(embed_dim, conv_out_channels, K)
+        if graph_conv.upper() == 'GCONV_ELMAN':
+            gconv = GConvElman(embed_dim, conv_out_channels,
+                               aggr="add", init_ones_=init_ones_, bias=bias, hidden_channels=conv_hidden_channels)
+        else:
+            # graph_conv == 'GCONV_GRU'
+            gconv = GConvGRU(embed_dim, conv_out_channels, K, bias=bias,
+                             init_ones_=init_ones_, normalization=normalization, )
         self.rnn_gconv = gconv
 
         self.debug = debug
@@ -43,17 +63,24 @@ class RGNN(nn.Module):
         # simple fully connected part for prediction
         dense = nn.Sequential()
         dense.add_module('lin0', nn.Linear(in_features=conv_out_channels * 2, out_features=dense_dims[0]))
-        dense.add_module('act0', self.act_fn)
+        #dense.add_module('act0', self.act_fn)
         #dense.add_module('dropout0', nn.Dropout(p=dropout_rate))
         for i in range(1, len(dense_dims)):
             dense.add_module(f'lin{i}', nn.Linear(in_features=dense_dims[i - 1], out_features=dense_dims[i]))
-            dense.add_module(f'act{i}', self.act_fn)
+            #dense.add_module(f'act{i}', self.act_fn)
             #dense.add_module(f'dropout{i}', nn.Dropout(p=dropout_rate))
         dense.add_module(f'lin{len(dense_dims)}',
                          nn.Linear(in_features=dense_dims[len(dense_dims) - 1], out_features=target_dim))
-        dense.add_module(f'act{len(dense_dims)}', self.act_fn)
+        #dense.add_module(f'act{len(dense_dims)}', self.act_fn)
         #dense.add_module(f'dropout{len(dense_dims)}', nn.Dropout(p=dropout_rate))
         self.dense_sequence = dense
+
+        if init_ones_:
+            for module in self.dense_sequence.children():
+                if isinstance(module, nn.Linear):
+                    nn.init.ones_(module.weight)
+                    if module.bias is not None:
+                        nn.init.zeros_(module.bias)
 
         self.out_layer = nn.Softmax(dim=0)
 
@@ -65,10 +92,9 @@ class RGNN(nn.Module):
 
         # graph convolution
         h = self.rnn_gconv(h, edge_index, edge_weight)
-        h = self.act_fn(h)
 
         # prediction
         h = torch.cat([h[home], h[away]], dim=0)
-        h = self.dense_sequence(h)
+        #h = self.dense_sequence(h)
         h = self.out_layer(h)
         return h
